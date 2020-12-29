b862e4bec66a4c3fa561ff03873ad20c8cdcf262
atharvdamle <54550557+atharvdamle@users.noreply.github.com>
2020-12-29 11:19:23 +0530

    Basic Routing

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const router = require('./routes/api/users');
 
 const app = express();
 
 connectDB();
 
 const PORT = process.env.PORT || 5000;
+
+//Defining Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 app.get('/', (req, res) => res.send('API Running'));
 
